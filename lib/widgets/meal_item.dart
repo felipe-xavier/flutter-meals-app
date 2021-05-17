c76c0ef2776f206851bd7ea1b0d0d8ff2b17a4df
felipe.xavier <felipe.uece@gmail.com>
2021-05-17 08:54:53 -0300

    Add MealItem widget.

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final MealComplexity complexity;
+  final MealAffordability affordability;
+
+  const MealItem({
+    required this.title,
+    required this.imageUrl,
+    required this.duration,
+    required this.complexity,
+    required this.affordability,
+  });
+
+  String get complexityText {
+    switch (complexity) {
+      case MealComplexity.Simple:
+        return 'Simple';
+      case MealComplexity.Challenging:
+        return 'Challenging';
+      case MealComplexity.Hard:
+        return 'Hard';
+      default:
+        return '';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case MealAffordability.Affordable:
+        return 'Affordable';
+      case MealAffordability.Pricey:
+        return 'Pricey';
+      case MealAffordability.Luxurious:
+        return 'Luxurious';
+      default:
+        return '';
+    }
+  }
+
+  void selectMeal() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  // right: 10,
+                  child: Container(
+                    width: 400,
+                    child: Text(
+                      title,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                        backgroundColor: Colors.black54,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('${duration}min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(width: 6),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6),
+                      Text(affordabilityText),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
